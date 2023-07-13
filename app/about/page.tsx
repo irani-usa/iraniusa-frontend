798aa9e72f6d05fadf2e46a135caa53c8734bb35
isopouya <pouya2402@gmail.com>
2023-07-13 23:16:25 +0330

    fix: add overlay menu on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,15 @@
 
+import OverlayMenu from '@/components/OverlayMenu'
 import React from 'react'
 
 const page = () => {
   return (
-   <h1 className='text-black container flex flex-col py-10 justify-center text-4xl font-outfit font-bold'>
+  <>
+  <OverlayMenu />
+  <h1 className='text-black container flex flex-col py-10 justify-center text-4xl font-outfit font-bold'>
     About Us
-   </h1>
+   </h1> 
+  </>
   )
 }
 
