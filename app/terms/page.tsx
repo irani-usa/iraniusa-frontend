292113471f73d738c89670e2efe4ec7076a9dabc
isopouya <pouya2402@gmail.com>
2023-07-14 10:33:57 +0330

    fix: Edit and finish terms page

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -7,9 +7,9 @@ const page = () => {
    <>
     <OverlayMenu />
     <section className='container py-8'>
-        <h1 className='text-black font-bold text-2xl py-5'>Terms & privacy policy</h1>
+        <h1 className='text-black font-bold text-2xl py-5 lg:text-5xl lg:py-28'>Terms & privacy policy</h1>
       <div className='py-4'>
-      <h3 className='text-primary-shade-10 text-xl font-semibold py-10 font-opensans'>What are Terms and Conditions?</h3>
+      <h3 className='text-primary-shade-10 text-xl font-bold py-10 font-opensans lg:text-2xl lg:font-bold'>What are Terms and Conditions?</h3>
         <p className='text-white-shade-9 text-lg font-medium font-opensans'>
         Terms and conditions outline what users can and cannot do with your website, <span className='text-primary-shade-10'>products, and services.</span> They lay out the rules to protect you in case of misuse and enable you to take action if it becomes necessary.
         </p>
@@ -18,7 +18,7 @@ const page = () => {
         </p>
       </div>
        <div className='py-5'>
-       <h1 className='text-blue text-xl font-bold py-12 font-opensans'>Are Terms and Conditions Required by Law?</h1>
+       <h1 className='text-blue text-xl font-bold py-12 font-opensans lg:text-2xl lg:font-bold'>Are Terms and Conditions Required by Law?</h1>
         <p className='text-white-shade-9 text-lg font-medium font-opensans'>
         No. You aren’t legally required to have a terms and conditions agreement on your website. However, having terms and conditions is a good practice as it sets the <span className='text-primary-shade-10'>“house”</span> rules for everybody and creates a <span className='text-primary-shade-10'>safer environment.</span> This is especially important in some western countries where frivolous lawsuits are somewhat of a norm.
         </p>
@@ -27,7 +27,7 @@ const page = () => {
         </p>
        </div>
      <div className='py-5'>
-     <h1 className='text-blue text-xl font-bold py-12 font-opensans'>Copyright & Intellectual Property</h1>
+     <h1 className='text-blue text-xl font-bold py-12 font-opensans lg:text-2xl lg:font-bold'>Copyright & Intellectual Property</h1>
         <p className='text-black text-lg font-medium font-opensans'>
         It also helps having a copyright notice that confirms and warns your users that the content on your website is yours and is not to be reproduced or re-used without your express permission. It re-affirms that you hold trademarks and ownership over certain elements of the website itself and over the products that you sell and should be a no-brainer.
         </p>
@@ -36,13 +36,13 @@ const page = () => {
         </p>
      </div>
   <div className='py-5'>
-  <h1 className='text-blue text-xl font-bold py-12 font-opensans'>DMCA Takedown Process</h1>
+  <h1 className='text-blue text-xl font-bold py-12 font-opensans lg:text-2xl lg:font-bold'>DMCA Takedown Process</h1>
         <p className='text-black text-lg font-medium font-opensans '>
         It also helps having a copyright notice that confirms and warns your users that the content on your website is yours and is not to be reproduced or re-used without your express permission. It re-affirms that you hold trademarks and ownership over certain elements of the website itself and over the products that you sell and should be a no-brainer.
         </p>
   </div>
   <div className='py-4'>
-      <h3 className='text-primary-shade-10 text-xl font-semibold py-10 font-opensans'>What are Terms and Conditions?</h3>
+      <h3 className='text-primary-shade-10 text-xl font-bold py-10 font-opensans lg:text-2xl lg:font-bold'>What are Terms and Conditions?</h3>
         <p className='text-white-shade-9 text-lg font-medium font-opensans'>
         Terms and conditions outline what users can and cannot do with your website, <span className='text-primary-shade-10'>products, and services.</span> They lay out the rules to protect you in case of misuse and enable you to take action if it becomes necessary.
         </p>
@@ -57,7 +57,7 @@ const page = () => {
         </p>
       </div>
       <div className='py-5'>
-       <h1 className='text-blue text-xl font-bold py-12 font-opensans'>Are Terms and Conditions Required by Law?</h1>
+       <h1 className='text-blue text-xl font-bold py-12 font-opensans lg:text-2xl lg:font-bold'>Are Terms and Conditions Required by Law?</h1>
         <p className='text-white-shade-9 text-lg font-medium font-opensans'>
         No. You aren’t legally required to have a terms and conditions agreement on your website. However, having terms and conditions is a good practice as it sets the <span className='text-primary-shade-10'>“house”</span> rules for everybody and creates a <span className='text-primary-shade-10'>safer environment.</span> This is especially important in some western countries where frivolous lawsuits are somewhat of a norm.
         </p>
@@ -66,7 +66,7 @@ const page = () => {
         </p>
        </div>
      <div className='py-5'>
-     <h1 className='text-blue text-xl font-bold py-12 font-opensans'>Copyright & Intellectual Property</h1>
+     <h1 className='text-blue text-xl font-bold py-12 font-opensans lg:text-2xl lg:font-bold'>Copyright & Intellectual Property</h1>
         <p className='text-black text-lg font-medium font-opensans'>
         It also helps having a copyright notice that confirms and warns your users that the content on your website is yours and is not to be reproduced or re-used without your express permission. It re-affirms that you hold trademarks and ownership over certain elements of the website itself and over the products that you sell and should be a no-brainer.
         </p>
@@ -75,7 +75,7 @@ const page = () => {
         </p>
      </div>
   <div className='py-5'>
-  <h1 className='text-blue text-xl font-bold py-12 font-opensans'>DMCA Takedown Process</h1>
+  <h1 className='text-blue text-xl font-bold py-12 font-opensans lg:text-2xl lg:font-bold'>DMCA Takedown Process</h1>
         <p className='text-black text-lg font-medium font-opensans '>
         It also helps having a copyright notice that confirms and warns your users that the content on your website is yours and is not to be reproduced or re-used without your express permission. It re-affirms that you hold trademarks and ownership over certain elements of the website itself and over the products that you sell and should be a no-brainer.
         </p>
