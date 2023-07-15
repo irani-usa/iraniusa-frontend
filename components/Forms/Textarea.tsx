95ab29e12f78a740873e90fe873ceccaab62aa49
isopouya <pouya2402@gmail.com>
2023-07-15 22:01:49 +0330

    fix: Edit contact page

--- components/Forms/Textarea.tsx
+++ components/Forms/Textarea.tsx
@@ -13,7 +13,7 @@ const Textarea: React.FC<TextareaProps> = (props) => {
           <div>
           <label className='block text-lg font-bold py-3 text-black' htmlFor={props.id}>{props.label}</label>
             
-            <textarea className={`${props.classes} w-full px-3 py-3 font-normal rounded-xl bg-white-shade-3 focus:outline-none focus:shadow-none`} rows={4} id={props.id} placeholder={props.placeHolder}>
+            <textarea className={`${props.classes} w-full text-black px-3 py-3 font-normal rounded-xl bg-white-shade-3 focus:outline-none focus:shadow-none`} rows={4} id={props.id} placeholder={props.placeHolder}>
                 
             </textarea>
           </div>
