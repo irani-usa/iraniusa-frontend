5e4c7dcc265be29f029d161859a1381cafb2f800
isopouya <pouya2402@gmail.com>
2023-07-17 13:27:32 +0330

    fix: Edit infobox in about page

--- components/Info/index.tsx
+++ components/Info/index.tsx
@@ -7,9 +7,9 @@ interface InfoProps {
 
 const Infobox: React.FC<InfoProps> = (props) => {
   return (
-    <div className={`${props.classes} py-14 px-16 rounded-2xl gap-y-3 flex flex-col items-center justify-center text-black`}>
-        <h1 className='text-5xl font-outfit font-bold'>{props.content}</h1>
-        <h4 className='text-white-shade-9 font-opensans text-2xl font-medium'>{props.info}</h4>
+    <div className={`${props.classes} py-14 px-20 rounded-2xl gap-y-3 flex flex-col items-center justify-center text-black`}>
+        <h1 className='text-4xl font-outfit font-bold'>{props.content}</h1>
+        <h4 className='text-white-shade-9 font-opensans text-xl font-medium'>{props.info}</h4>
     </div>
   )
 }
