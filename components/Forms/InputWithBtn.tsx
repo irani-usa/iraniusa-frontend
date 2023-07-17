f33f240c9f903d83808604e8b9819199a82aa2c3
Milad Mohamadi <MiladDevarts@gmail.com>
2023-07-17 16:26:38 +0330

    feat: create input with button component

---
+++ components/Forms/InputWithBtn.tsx
@@ -0,0 +1,23 @@
+
+import React from 'react'
+
+import { Search } from '../Icons/Outline'
+import Button from '../Button'
+
+interface ButtonProps {
+    placeHolder:string,
+    classes: string,
+    btnContent:string
+}
+
+const InputWithBtn:React.FC<ButtonProps> = (props) => {
+  return (
+    <div className={`${props.classes} flex justify-between items-center gap-x-2 bg-white-shade-3 rounded-xl pl-5 w-full lg:w-2/6 h-20`}>
+        <Search/>
+        <input type="text" placeholder={props.placeHolder} className='font-opensans w-7/12 h-full bg-white-shade-3 focus:outline-none focus:shadow-none text-black' />
+        <Button content={props.btnContent} classes={'w-3/12 h-[90%]'}/>
+    </div>
+  )
+}
+
+export default InputWithBtn
