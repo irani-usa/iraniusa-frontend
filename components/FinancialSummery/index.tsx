7902035b253f6cc6e65a54b1972d47ad7f434cf5
Milad Mohamadi <MiladDevarts@gmail.com>
2023-07-24 04:51:28 +0330

    feat: add financial summery illustration

--- components/FinancialSummery/index.tsx
+++ components/FinancialSummery/index.tsx
@@ -6,8 +6,8 @@ import Image from 'next/image'
 const FinancialSummery = () => {
   return (
     <section className='container my-36'>
-      <div className='flex justify-between w-full border-b-2 border-white-shade-2'>
-        <div className='w-9/12 h-full flex flex-col items-start gap-y-4'>
+      <div className='flex flex-col gap-y-8 lg:flex-row justify-between w-full border-b-2 border-white-shade-2'>
+        <div className='order-2 lg:order-2 w-9/12 h-full flex flex-col items-start gap-y-4'>
           <Star />
           <h3 className='text-xl lg:text-3xl leading-8 text-black font-semibold font-outfit text-center'>
             Financial markets
@@ -16,8 +16,8 @@ const FinancialSummery = () => {
             is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
           </p>
         </div>
-        <div className='w-3/12 h-full'>
-          <Image src={"/images/financial/coin.png"} className='w-full h-full' width={200} height={100} alt='coin' />
+        <div className='flex justify-end order-1 lg:order-2 w-1/2 lg:w-3/12 h-full self-end'>
+          <Image src={"/images/financial/coin.png"} className='w-full h-full self-end' width={200} height={100} alt='coin' />
         </div>
       </div>
     </section>
