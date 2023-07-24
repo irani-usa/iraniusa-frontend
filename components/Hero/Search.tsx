accf4895c30ffc0b3ed40d64d5d312e492f7bd95
Milad Mohamadi <MiladDevarts@gmail.com>
2023-07-24 14:54:26 +0330

    fix: add sample seach-bar in hero

---
+++ components/Hero/Search.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Search = () => {
+  return (
+    <section className='flex justify-center items-center my-20 rounded-2xl'>
+        <div className='w-1/2 h-36 bg-white-shade-3'>
+            
+        </div>
+    </section>
+  )
+}
+
+export default Search
--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import Title from './Title'
+import Search from './Search'
 
 const Hero = () => {
   return (
     <section className='container'> 
         <Title/>
+        <Search/>
     </section>
   )
 }
