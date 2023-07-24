851f2137971aa862bea41c94a797f30fcd187dff
Milad Mohamadi <MiladDevarts@gmail.com>
2023-07-24 15:15:13 +0330

    fix: edit search property

--- components/Hero/Search.tsx
+++ components/Hero/Search.tsx
@@ -2,13 +2,19 @@
 import React from 'react'
 
 const Search = () => {
-  return (
-    <section className='flex justify-center items-center my-20 rounded-2xl'>
-        <div className='w-1/2 h-36 bg-white-shade-3 rounded-2xl'>
-            
-        </div>
-    </section>
-  )
+    return (
+        <section className='flex flex-col gap-y-8 justify-center items-center my-16 rounded-2xl'>
+            <div className='w-10/12 h-36 bg-white-shade-3 rounded-2xl'>
+
+            </div>
+            <span className='flex flex-col lg:flex-row items-center font-open-sans text-white-shade-11'>
+                <span className='text-black ml-2'>
+                Popular searches:
+                </span>
+                <span>Lawyer,Restaurant,plumber,programmer</span>
+            </span>
+        </section>
+    )
 }
 
 export default Search
