94cab12c0df7027befdc499c40544286a0af69c4
Milad Mohamadi <MiladDevarts@gmail.com>
2023-07-24 18:04:24 +0330

    fix: make menu activation dynamic

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -1,11 +1,28 @@
 
-import React from 'react'
+import React, { useState } from 'react'
+import Link from 'next/link'
+
+import { usePathname } from 'next/navigation'
+
 import Logo from '../Logo'
 import { User } from '../Icons/Solid'
-import Link from 'next/link'
 import HamburgerNav from '../Hamburger'
 
 const Navigation = () => {
+
+  const [menuItems, setMenuItesm] = useState([
+    { value: 'Home', slug: '/' },
+    { value: 'Events', slug: '/events' },
+    { value: 'Blog', slug: '/blog' },
+    { value: 'Financial market', slug: '/financial-market' },
+    { value: 'ADS reservation', slug: '/ads-reservationt' },
+    { value: 'Music player', slug: '/music-player' },
+    { value: 'Plans', slug: '/plans' },
+    { value: 'About', slug: '/about' },
+  ])
+
+  const pathname = usePathname()
+
   return (
     <nav className='w-full h-24 border-b border-white-shade-4'>
       <div className='container flex justify-between items-center h-full'>
@@ -14,55 +31,28 @@ const Navigation = () => {
             <Logo width={80} height={200} />
           </Link>
           <ul className='navigation hidden h-full lg:flex items-center gap-x-10 text-white-shade-10 font-opensans font-medium'>
-            <li className='active'>
-              <Link href={"/"}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link href={"#"}>
-                Event
-              </Link>
-            </li>
-            <li>
-              <Link href={"#"}>
-                Blog
-              </Link>
-            </li>
-            <li>
-              <Link href={"/financial-market"}>
-                Financial market
-              </Link>
-            </li>
-            <li>
-              <Link href={"#"}>
-                ADS reservation
-              </Link>
-            </li>
-            <li>
-              <Link href={"#"}>
-                Music player
-              </Link>
-            </li>
-            <li>
-              <Link href={"#"}>
-                Plans
-              </Link>
-            </li>
-            <li>
-              <Link href={"/about"}>
-                About
-              </Link>
-            </li>
+            {
+              menuItems.map((link) => {
+                const isActive = pathname.startsWith(link.slug)
+                return (
+                  <li className={isActive ? 'active' : 'normal'}>
+                    <Link href={link.slug}>
+                      {link.value}
+                    </Link>
+                  </li>
+                )
+              })
+            }
           </ul>
         </div>
         <Link className='hidden lg:flex bg-white-shade-3 p-4 jcc aic rounded-full' href={"/login"}>
           <User classes={''} />
         </Link>
-        <HamburgerNav/>
+        <HamburgerNav />
       </div>
     </nav>
   )
+
 }
 
 export default Navigation
