67175075d3d0a1d61185d1cd845ea960867c3e0c
Milad Mohamadi <MiladDevarts@gmail.com>
2023-07-24 20:54:48 +0330

    fix: re-size plan info

--- components/Home/PlanInfo/Card.tsx
+++ components/Home/PlanInfo/Card.tsx
@@ -4,20 +4,20 @@ import React from 'react'
 interface CardProps {
   content: string,
   info: string,
-  classes:string,
+  classes: string,
 }
 
 
-const Card: React.FC<CardProps> = (props) =>  {
+const Card: React.FC<CardProps> = (props) => {
   return (
     <>
-    <section className='flex flex-col items-center gap-y-2'>
-    <div className='bg-white-shade-3 flex flex-col items-center py-16 w-[265px] rounded-2xl'>
-      <h1 className='text-black text-[26px] font-outfit font-bold'>{props.content}</h1>
-      <h4 className='text-white-shade-9 font-opensans text-[18px] font-semibold'>{props.info}</h4>
-    </div>
-    <div className={`${props.classes} h-[7px]  w-36 rounded-[18px]`}></div>
-    </section>
+      <section className='flex flex-col items-center gap-y-2'>
+        <div className='bg-white-shade-3 flex flex-col items-center py-16 w-[265px] rounded-2xl'>
+          <h1 className='text-black text-[26px] font-outfit font-semibold'>{props.content}</h1>
+          <h4 className='text-white-shade-9 font-opensans text-[18px] font-semibold'>{props.info}</h4>
+        </div>
+        <div className={`${props.classes} h-[7px]  w-36 rounded-[18px]`}></div>
+      </section>
     </>
   )
 }
--- components/Home/PlanInfo/index.tsx
+++ components/Home/PlanInfo/index.tsx
@@ -4,14 +4,14 @@ import Card from './Card'
 
 const PlanInfo = () => {
   return (
-   <section className='container'>
-    <div className='w-full flex items-center overflow-x-scroll no-scroll gap-x-5 lg:justify-between'>
-    <Card content={'Business Ultra'} info={'Free and pro accounts'} classes={'bg-[#B80042]'} />
-     <Card content={'Artists & Events'} info={'Free and pro accounts'} classes={'bg-[#9949FF]'} />
-     <Card content={'Professional'} info={'Free and pro accounts'} classes={'bg-[#FFA841]'}/>
-     <Card content={'Business Platinum'} info={'Free and pro accounts'} classes={'bg-[#1F78FF]'}/>
-    </div>
-   </section>
+    <section className='container'>
+      <div className='w-full flex items-center overflow-x-scroll no-scroll gap-x-3 lg:gap-x-6 lg:justify-center'>
+        <Card content={'Business Ultra'} info={'Free and pro accounts'} classes={'bg-[#B80042]'} />
+        <Card content={'Artists & Events'} info={'Free and pro accounts'} classes={'bg-[#9949FF]'} />
+        <Card content={'Professional'} info={'Free and pro accounts'} classes={'bg-[#FFA841]'} />
+        <Card content={'Business Platinum'} info={'Free and pro accounts'} classes={'bg-[#1F78FF]'} />
+      </div>
+    </section>
   )
 }
 
