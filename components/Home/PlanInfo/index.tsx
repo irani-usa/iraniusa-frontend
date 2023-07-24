f3eef6519e01724a7740f0a66ba830e01dda4c55
isopouya <pouya2402@gmail.com>
2023-07-24 20:47:50 +0330

    fix: Edit plan box

--- components/Home/PlanInfo/index.tsx
+++ components/Home/PlanInfo/index.tsx
@@ -5,7 +5,7 @@ import Card from './Card'
 const PlanInfo = () => {
   return (
    <section className='container'>
-    <div className='w-full flex items-center overflow-x-scroll no-scroll gap-x-5'>
+    <div className='w-full flex items-center overflow-x-scroll no-scroll gap-x-5 lg:justify-between'>
     <Card content={'Business Ultra'} info={'Free and pro accounts'} classes={'bg-[#B80042]'} />
      <Card content={'Artists & Events'} info={'Free and pro accounts'} classes={'bg-[#9949FF]'} />
      <Card content={'Professional'} info={'Free and pro accounts'} classes={'bg-[#FFA841]'}/>
