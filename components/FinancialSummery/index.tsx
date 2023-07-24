b3b196e0ee89da26a583202e72d57bc39e5804f6
Milad Mohamadi <MiladDevarts@gmail.com>
2023-07-24 04:57:51 +0330

    fix: edit spacing in financial-summery

--- components/FinancialSummery/index.tsx
+++ components/FinancialSummery/index.tsx
@@ -1,13 +1,17 @@
 
 import React from 'react'
-import Star from '../Icons/Solid/Star'
 import Image from 'next/image'
+
+import Star from '../Icons/Solid/Star'
+import Financial from '../Financial'
+
+import { Btc, Dollar, Game, Google, Oil } from '../Icons/Solid'
 
 const FinancialSummery = () => {
   return (
     <section className='container my-36'>
       <div className='flex flex-col gap-y-8 lg:flex-row justify-between w-full border-b-2 border-white-shade-2'>
-        <div className='order-2 lg:order-2 w-9/12 h-full flex flex-col items-start gap-y-4'>
+        <div className='order-2 lg:order-2 w-9/12 h-full flex flex-col items-start gap-y-4 pb-5'>
           <Star />
           <h3 className='text-xl lg:text-3xl leading-8 text-black font-semibold font-outfit text-center'>
             Financial markets
@@ -20,6 +24,63 @@ const FinancialSummery = () => {
           <Image src={"/images/financial/coin.png"} className='w-full h-full self-end' width={200} height={100} alt='coin' />
         </div>
       </div>
+      <div className="flex overflow-x-scroll no-scroll items-center gap-x-5 my-6">
+          <Financial
+            content={"Price"}
+            content2={"24h%"}
+            classes={"text-black border-[1px] border-white-shade-4 w-2/3"}
+            frame={"border-[1.5px] border-white-shade-4"}
+            title={"BTC"}
+            price={"$36,641.20"}
+            found={"+6.04%"}
+            iconComp={<Btc />}
+            info={true}
+          />
+          <Financial
+            content={"Price"}
+            content2={"Total return"}
+            classes={"text-black border-[1px] border-white-shade-4 w-2/3"}
+            frame={"border-[1.5px] border-white-shade-4"}
+            title={"Oil"}
+            price={"$310,40"}
+            found={"-1,10%"}
+            iconComp={<Oil />}
+            info={true}
+          />
+          <Financial
+            content={"Price"}
+            content2={"Total return"}
+            classes={"text-black border-[1px] border-white-shade-4 w-2/3"}
+            frame={"border-[1.5px] border-white-shade-4"}
+            title={"Dollar"}
+            price={"$310,40"}
+            found={"-1,10%"}
+            iconComp={<Dollar />}
+            info={true}
+          />
+          <Financial
+            content={"Price"}
+            content2={"Total return"}
+            classes={"text-black border-[1px] border-white-shade-4 w-2/3"}
+            frame={"border-[1.5px] border-white-shade-4"}
+            title={"Google"}
+            price={"$36,641.20"}
+            found={"+6.04%"}
+            iconComp={<Google />}
+            info={true}
+          />
+          <Financial
+            content={"Price"}
+            content2={"Total return"}
+            classes={"text-black border-[1px] border-white-shade-4 w-2/3"}
+            frame={"border-[1.5px] border-white-shade-4"}
+            title={"Game"}
+            price={"$36,641.20"}
+            found={"+6.04%"}
+            iconComp={<Game />}
+            info={true}
+          />
+        </div>
     </section>
   )
 }
