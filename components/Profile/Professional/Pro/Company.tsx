c6a0c0326caae9f3456890919b577cace912eb8b
isopouya <pouya2402@gmail.com>
2023-09-22 18:50:31 +0330

    fix: Edit company section

--- components/Profile/Professional/Pro/Company.tsx
+++ components/Profile/Professional/Pro/Company.tsx
@@ -6,8 +6,9 @@ const Company = () => {
     <div className='container flex flex-col gap-y-6 mt-8 lg:px-0'>
     <h1 className="text-[26px] font-outfit font-bold text-center text-black lg:text-3xl">Companies i worked with</h1>
          
-          <section>
-            <div className='bg-white-shade-2 py-9 flex items-center justify-center w-[60%]'><svg width="78" height="69" viewBox="0 0 78 69" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <section className='flex items-center gap-x-7 py-5'>
+        
+<div className='bg-white-shade-2 py-9 flex items-center justify-center rounded-3xl w-[75%]'><svg width="78" height="69" viewBox="0 0 78 69" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M0.612305 34.2635V68.527H10.7683H20.9248L20.7234 47.2642C20.5862 32.7613 20.6797 26.465 21.0173 27.4594C21.2893 28.2613 23.6957 37.7749 26.3646 48.6007L31.2171 68.284L39.193 68.4172L47.169 68.5504L51.8628 47.7619L56.5567 26.9734L56.6865 47.7502L56.8158 68.527H67.0051H77.1939V34.2635V0H62.5743H47.9547L46.2157 7.16861C45.2594 11.1116 43.2887 19.3042 41.8361 25.3744C40.3835 31.4452 39.1269 36.3456 39.0439 36.2639C38.9603 36.1828 37.1084 28.482 34.9278 19.1516C32.7477 9.82124 30.8437 1.69519 30.697 1.09352L30.4304 0H15.5214H0.612305V34.2635Z" fill="#F2F2F2"/>
 </svg>
 </div>
