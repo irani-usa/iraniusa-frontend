0d5a61e52e1b45956b9d070a2acebcd43df6a814
isopouya <pouya2402@gmail.com>
2023-09-22 11:03:38 +0330

    fix: Add profile comps

--- components/Profile/Professional/Free/Experinces.tsx
+++ components/Profile/Professional/Free/Experinces.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 
 const Experinces = () => {
   return (
-    <div>Experinces</div>
+    <div className='flex flex-col gap-y-6 w-full border-white-shade-4 border-b pb-8'>
+      <h1 className="text-[26px] font-outfit font-bold text-center text-black">Experiences and projects</h1>
+            <p className="text-center text-white-shade-9  font-opensans max-w-sm ">is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+    </div>
   )
 }
 
--- components/Profile/general/Cover.tsx
+++ components/Profile/general/Cover.tsx
@@ -1,10 +1,15 @@
 
 import React from 'react'
+import Avatar from './Avatar'
+import UserTitle from './UserTitle'
 
 const Cover = () => {
   return (
-    <div  style={{ backgroundImage: `url('/images/profile/Cover.png')` }} className='w-full bg-cover h-24 '>
-
+    <div  style={{ backgroundImage: `url('/images/profile/Cover.png')` }} className='w-full bg-cover h-24 relative'>
+     <div className='container'>
+     <Avatar />
+   
+     </div>
     </div>
   )
 }
