c077a3e10672bee64458c1e62a816b4e5660b753
isopouya <pouya2402@gmail.com>
2023-09-24 01:44:02 +0330

    feat: create gallery and frequent section

---
+++ components/Profile/business/pro/Frequent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Frequent = () => {
+  return (
+    <section className='w-full border-b border-white-shade-4 py-8 lg:py-11'>
+        <div className='container lg:px-0'>
+        <h1 className="text-black font-bold text-2xl font-outfit mt-5 lg:text-3xl">Most frequent questions </h1>
+        </div>
+    </section>
+  )
+}
+
+export default Frequent
