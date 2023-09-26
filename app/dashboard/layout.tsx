231b544795ac6659483833a6bf1a2cdc75c895b1
Milad Mohamadi <MiladDevarts@gmail.com>
2023-09-27 00:10:35 +0400

    fix resize unsopportable screen

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,20 +1,22 @@
-import Header from "@/components/Dashboard/Layout/Header"
-import SideMenu from "@/components/Dashboard/Layout/SideMenu"
+import Header from "@/components/Dashboard/Layout/Header";
+import SideMenu from "@/components/Dashboard/Layout/SideMenu";
+import Unsupportable from "@/components/Dashboard/Layout/unsupportable";
 
 export default function DashboardLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <main className="flex w-full">
-      <SideMenu />
-      <section className="flex flex-col w-4/5 h-screen">
-        <Header />
-        <main>
-          {children}
-        </main>
-      </section>
-    </main>
-  )
+    <>
+      <Unsupportable />
+      <main className="hidden lg:flex w-full">
+        <SideMenu />
+        <section className="flex flex-col w-4/5 h-screen">
+          <Header />
+          <main>{children}</main>
+        </section>
+      </main>
+    </>
+  );
 }
