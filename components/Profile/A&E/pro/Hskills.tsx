06119ad407a8e47a45468db2897fafcaa781538b
isopouya <pouya2402@gmail.com>
2023-09-27 19:42:51 +0330

    fix: reasponsive

---
+++ components/Profile/A&E/pro/Hskills.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hskills = () => {
+  return (
+    <section className='border-b border-white-shade-4 pb-11'>
+    <div className="container mt-7 lg:pl-5">
+         <h1 className="text-2xl text-black font-outfit font-bold lg:text-3xl">Hard skills</h1>
+         <ul className='list-disc mt-7 flex items-center gap-x-12 text-lg font-medium lg:gap-x-6 text-black pl-5'>
+            <li>Song writer</li>
+            <li>Musician</li>
+            <li>Composer</li>
+         </ul>
+      </div>
+ </section>
+  )
+}
+
+export default Hskills
