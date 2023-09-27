3fd1a524523b16fc4cd1e32907b5f68f72e1e73b
isopouya <pouya2402@gmail.com>
2023-09-28 00:24:37 +0330

    fix: add bg

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -192,74 +192,65 @@ const page = () => {
           </h1>
         </div>
         <div className="lg:flex gap-x-6">
-<<<<<<< HEAD
-          <PopularConcert content={"Shadmehr aghili"} concert={"Dumbarton concerts"} date={"11/7/16"} city={"Washington DC"} classes={""} price={"$67"} background={"/images/events/shadmehr.webp"} />
-          <PopularConcert content={"Shadmehr aghili"} concert={"Dumbarton concerts"} date={"11/7/16"} city={"Washington DC"} classes={"hidden lg:flex"} price={"$67"} background={"/images/events/shadmehr.webp"} />
-          <PopularConcert content={"Shadmehr aghili"} concert={"Dumbarton concerts"} date={"11/7/16"} city={"Washington DC"} classes={"hidden lg:flex"} price={"$67"} background={"/images/events/shadmehr.webp"} />
-=======
           <PopularConcert
             content={"Shadmehr aghili"}
             concert={"Dumbarton concerts"}
             date={"11/7/16"}
             city={"Washington DC"}
             classes={""}
-            price={"$67"}
-          />
+            price={"$67"} background={"/images/events/shadmehr.webp"}          />
           <PopularConcert
             content={"Shadmehr aghili"}
             concert={"Dumbarton concerts"}
             date={"11/7/16"}
             city={"Washington DC"}
             classes={"hidden lg:flex"}
-            price={"$67"}
-          />
+            price={"$67"} background={"/images/events/shadmehr.webp"}          />
           <PopularConcert
             content={"Shadmehr aghili"}
             concert={"Dumbarton concerts"}
             date={"11/7/16"}
             city={"Washington DC"}
             classes={"hidden lg:flex"}
-            price={"$67"}
-          />
->>>>>>> 57d549dce2e68c3ff7230b2291c761ed5c64a0b5
-        </div>
-        <section className="flex justify-center gap-x-3 mt-12 lg:hidden">
-          <div className="flex items-center justify-center border-[1px] border-white-shade-4 py-5 px-6 rounded-full">
-            <svg
-              width="8"
-              height="14"
-              viewBox="0 0 8 14"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M6.59961 13.0667L1.39961 6.99998L6.59961 0.933317"
-                stroke="black"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          </div>
-          <div className="flex items-center justify-center border-[1px] border-white-shade-4 py-5 px-6 rounded-full">
-            <svg
-              width="8"
-              height="14"
-              viewBox="0 0 8 14"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M1.40039 0.93335L6.60039 7.00002L1.40039 13.0667"
-                stroke="black"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
-          </div>
-        </section>
-      </section>
+            price={"$67"} background={"/images/events/shadmehr.webp"}          />
+        </div >
+  <section className="flex justify-center gap-x-3 mt-12 lg:hidden">
+    <div className="flex items-center justify-center border-[1px] border-white-shade-4 py-5 px-6 rounded-full">
+      <svg
+        width="8"
+        height="14"
+        viewBox="0 0 8 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M6.59961 13.0667L1.39961 6.99998L6.59961 0.933317"
+          stroke="black"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+    <div className="flex items-center justify-center border-[1px] border-white-shade-4 py-5 px-6 rounded-full">
+      <svg
+        width="8"
+        height="14"
+        viewBox="0 0 8 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1.40039 0.93335L6.60039 7.00002L1.40039 13.0667"
+          stroke="black"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+    </div>
+  </section>
+      </section >
     </>
   );
 };
