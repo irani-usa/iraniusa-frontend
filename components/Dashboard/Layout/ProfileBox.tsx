9119dd7abfcdcf6dbb217898ea4160058e212a21
Milad Mohamadi <MiladDevarts@gmail.com>
2023-09-28 13:40:51 +0400

    feat: add genera/profile-box in side-menu

---
+++ components/Dashboard/Layout/ProfileBox.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ProfileBox = () => {
+  return (
+    <section className="flex items-center gap-x-4 px-12">
+      <div
+        className="w-20 h-16 rounded-lg bg-center bg-cover"
+        style={{
+          backgroundImage: `url('/images/dashboard/sidemenu/user-profile.png')`,
+        }}
+      ></div>
+      <div className="flex flex-col gap-y-3">
+        <h4 className="text-black font-outfit text-lg">Amin gholami</h4>
+        <span className="text-white-shade-11">Product designer</span>
+      </div>
+    </section>
+  );
+};
+
+export default ProfileBox;
--- components/Dashboard/Layout/SideMenu.tsx
+++ components/Dashboard/Layout/SideMenu.tsx
@@ -1,3 +1,4 @@
+
 import React from "react";
 import Link from "next/link";
 
@@ -12,77 +13,81 @@ import {
   Support,
   Verified,
 } from "../Icons";
+import ProfileBox from "./ProfileBox";
 
 const SideMenu = () => {
   return (
     <>
-      <nav className="flex flex-col w-1/5 h-screen py-8  border-r border-white-shade-4">
-        <Link href={"/"} className="px-12">
-          <Logo />
-        </Link>
-        <div className="w-full border-b py-4 border-[#EDEDED] mt-4">
-          <div className="px-12">
-            <span className="inline-block mb-6 text-white-shade-8">Menu</span>
-            <div className="flex flex-col gap-y-2 font-medium">
-              <Link
-                className="flex items-center gap-x-2 px-6 py-4 bg-primary-tint-4 text-white text-lg rounded-2xl"
-                href={"/"}
-              >
-                <Profile />
-                Edit profile
-              </Link>
-              <Link
-                className="flex items-center gap-x-2 py-4  text-white-shade-8 text-lg rounded-2xl"
-                href={"/"}
-              >
-                <Reserve />
-                ADS reservation
-              </Link>
-              <Link
-                className="flex items-center gap-x-2 py-4  text-white-shade-8 text-lg rounded-2xl"
-                href={"/"}
-              >
-                <Connected />
-                My ads
-              </Link>
-              <Link
-                className="flex items-center gap-x-2 py-4  text-white-shade-8 text-lg rounded-2xl"
-                href={"/"}
-              >
-                <Book />
-                Blog
-              </Link>
-              <Link
-                className="flex items-center gap-x-2 py-4  text-white-shade-8 text-lg rounded-2xl"
-                href={"/"}
-              >
-                <Verified />
-                Verify
-              </Link>
+      <nav className="flex flex-col justify-between w-1/5 h-screen py-8  border-r border-white-shade-4">
+        <div className="w-full flex flex-col">
+          <Link href={"/"} className="px-12">
+            <Logo />
+          </Link>
+          <div className="w-full border-b py-4 border-[#EDEDED] mt-4">
+            <div className="px-12">
+              <span className="inline-block mb-6 text-white-shade-8">Menu</span>
+              <div className="flex flex-col gap-y-2 font-medium">
+                <Link
+                  className="flex items-center gap-x-2 px-6 py-4 bg-primary-tint-4 text-white text-lg rounded-2xl"
+                  href={"/"}
+                >
+                  <Profile />
+                  Edit profile
+                </Link>
+                <Link
+                  className="flex items-center gap-x-2 py-4  text-white-shade-8 text-lg rounded-2xl"
+                  href={"/"}
+                >
+                  <Reserve />
+                  ADS reservation
+                </Link>
+                <Link
+                  className="flex items-center gap-x-2 py-4  text-white-shade-8 text-lg rounded-2xl"
+                  href={"/"}
+                >
+                  <Connected />
+                  My ads
+                </Link>
+                <Link
+                  className="flex items-center gap-x-2 py-4  text-white-shade-8 text-lg rounded-2xl"
+                  href={"/"}
+                >
+                  <Book />
+                  Blog
+                </Link>
+                <Link
+                  className="flex items-center gap-x-2 py-4  text-white-shade-8 text-lg rounded-2xl"
+                  href={"/"}
+                >
+                  <Verified />
+                  Verify
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="w-full mt-8">
+            <div className="px-12">
+              <span className="inline-block mb-6 text-white-shade-8">General</span>
+              <div className="flex flex-col gap-y-2 font-medium">
+                <Link
+                  className="flex items-center gap-x-2 py-4 text-white-shade-8 text-lg rounded-2xl"
+                  href={"/"}
+                >
+                  <Support />
+                  Help
+                </Link>
+                <Link
+                  className="flex items-center gap-x-2 py-4 text-[#DE000D] text-lg rounded-2xl"
+                  href={"/"}
+                >
+                  <Exit />
+                  Log out
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-        <div className="w-full mt-8">
-          <div className="px-12">
-            <span className="inline-block mb-6 text-white-shade-8">Menu</span>
-            <div className="flex flex-col gap-y-2 font-medium">
-              <Link
-                className="flex items-center gap-x-2 py-4 text-white-shade-8 text-lg rounded-2xl"
-                href={"/"}
-              >
-                <Support />
-                Help
-              </Link>
-              <Link
-                className="flex items-center gap-x-2 py-4 text-[#DE000D] text-lg rounded-2xl"
-                href={"/"}
-              >
-                <Exit />
-                Log out
-              </Link>
-            </div>
-          </div>
-        </div>
+        <ProfileBox />
       </nav>
     </>
   );
