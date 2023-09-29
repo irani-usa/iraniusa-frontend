e9402ee34006e8782f8789fe311b93e8ad383769
isopouya <pouya2402@gmail.com>
2023-09-29 19:26:51 +0330

    fix: add props

---
+++ components/Pages/Blog/index.tsx
@@ -0,0 +1,9 @@
+import BlogCard from "./BlogCard";
+import BlogCardM from "./BlogCardM";
+import Bloggers from "./Bloggers";
+import Category from "./Category";
+import LawyarsCard from "./LawyarsCard";
+import PopularUser from "./PopulerUser";
+import Tag from "./Tag";
+
+export{BlogCard,BlogCardM,Bloggers,Category,LawyarsCard,PopularUser,Tag}
