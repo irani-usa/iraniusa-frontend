2d46629d38b8db57fbea61109d44a99fb68704ac
isopouya <pouya2402@gmail.com>
2023-09-29 19:28:19 +0330

    fix: add props

--- components/Profile/business/starter/Exprince.tsx
+++ components/Profile/business/starter/Exprince.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const Exprince = () => {
+interface ExprinceProps {
+  exprince:string,
+}
+
+const Exprince: React.FC<ExprinceProps> = (props) =>  {
   return (
     <section className='w-full border-b border-white-shade-4 py-8 lg:py-11'>
         <div className='container lg:pl-6'>
         <h1 className="text-black font-bold text-2xl font-outfit mt-5 lg:text-3xl">Company's experiences </h1>
-                <p className=" text-white-shade-9  font-opensans py-4 mt-3">Adidas has more than 20 years of experience in athletics industry through designing shoes and clothes also more than 20 collaborations wordwide </p>
+                <p className=" text-white-shade-9  font-opensans py-4 mt-3">{props.exprince} </p>
         </div>
     </section>
   )
