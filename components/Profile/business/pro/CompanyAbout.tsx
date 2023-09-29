b7bb271350ed2ecf1f594565bb8f589c90c44a3e
isopouya <pouya2402@gmail.com>
2023-09-29 19:27:31 +0330

    fix: add props

--- components/Profile/business/pro/CompanyAbout.tsx
+++ components/Profile/business/pro/CompanyAbout.tsx
@@ -1,7 +1,20 @@
 import Link from 'next/link'
 import React from 'react'
 
-const CompanyAbout = () => {
+
+interface CompanyAboutProps {
+  about:string,
+  city:string,
+  name:string,
+  job:string,
+  cover:string,
+  compimg:string,
+  compimg2:string,
+}
+
+
+
+const CompanyAbout: React.FC<CompanyAboutProps> = (props) => {
   return (
     <section className=" border-b border-white-shade-4 ">
     <div className="container lg:pl-5 py-7 flex flex-col gap-y-6">
@@ -9,37 +22,35 @@ const CompanyAbout = () => {
       What is our company all about?
     </h1>
     <p className=" text-white-shade-9  font-opensans">
-      Adidas has more than 20 years of experience in athletics industry
-      through designing shoes and clothes also more than 20 collaborations
-      wordwide{" "}
+{props.about}
     </p>
     <p className="text-lg text-black font-semibold font-opensans mt-4 lg:mt-0">
-      Based in Los angless
+      Based in {props.city}
     </p>
 
     <div className=" flex items-center gap-x-7">
       <div className="flex items-center gap-x-4">
         <div
           className="bg-cover bg-center w-14 h-14 rounded-full lg:w-20 lg:h-20"
-          style={{ backgroundImage: `url('/images/blog/User.webp')` }}
+          style={{ backgroundImage: `url(${props.cover})` }}
         ></div>
         <h1 className="text-black font-semibold text-lg font-opensans flex flex-col items-start gap-y-2 lg:text-xl lg:font-bold">
-          Jason todd
+         {props.name}
           <span className="font-opensans text-white-shade-8 font-normal text-sm">
-            Founder
+          {props.job}
           </span>
         </h1>
       </div>
       <Link
-        className="text-black bg-white-shade-3 py-4 text-lg rounded-2xl font-medium px-7 lg:py-3"
+        className="text-black bg-white-shade-3 py-4 text-lg rounded-2xl font-medium w-36 flex items-center justify-center lg:py-3"
         href={"#"}
       >
         View Profile
       </Link>
     </div>
     <section className='grid grid-cols-1 gap-y-5 py-5 lg:grid-cols-6 gap-x-5'>
-    <div style={{ backgroundImage: `url('/images/profile/business/about.png')` }} className='bg-cover bg-center w-full h-48 rounded-2xl  lg:col-span-4 lg:h-44'></div>
-    <div style={{ backgroundImage: `url('/images/profile/business/about.png')` }} className='bg-cover bg-center w-full h-48 rounded-2xl  lg:col-span-2 lg:h-44'></div>
+    <div style={{ backgroundImage: `url(${props.compimg})` }} className='bg-cover bg-center w-full h-48 rounded-2xl  lg:col-span-4 lg:h-44'></div>
+    <div style={{ backgroundImage: `url(${props.compimg2})` }} className='bg-cover bg-center w-full h-48 rounded-2xl  lg:col-span-2 lg:h-44'></div>
   </section>
   </div>
   </section>
