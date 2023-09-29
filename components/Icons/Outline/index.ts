25bd4205ea9518a7ebc8751accd5abf586bbd858
isopouya <pouya2402@gmail.com>
2023-09-29 19:26:35 +0330

    fix: add props

--- components/Icons/Outline/index.ts
+++ components/Icons/Outline/index.ts
@@ -11,12 +11,15 @@ import CoffeeShop from "./CoffeeShop";
 
 import Sort from "./Sort";
 import Currency from "./Currency";
+import Message from "./Message";
+import Location from "./Location";
+
 
 import Star from "./Star";
 import Gem from "./Gem";
 import Crown from "./Crown"
 import Music from "./Music";
+import Call from "./Call";
 
 
-
-export { Dots, Email, Password, Search, Resturant, Liquor, Furniture, CoffeeShop, Sort, Currency, Star ,Gem,Music,Crown }
+export {Call,Location,Message, Dots, Email, Password, Search, Resturant, Liquor, Furniture, CoffeeShop, Sort, Currency, Star ,Gem,Music,Crown }
---
+++ components/Pages/About/index.tsx
@@ -0,0 +1,4 @@
+import Stamp from "./Stamp";
+import Plan from "./Plans";
+
+export{Stamp,Plan}
