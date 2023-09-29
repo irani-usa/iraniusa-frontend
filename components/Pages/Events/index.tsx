fc285081efe7f6d221566bf10509a4f9ba3ef767
isopouya <pouya2402@gmail.com>
2023-09-29 19:27:16 +0330

    fix: add props

---
+++ components/Pages/Events/index.tsx
@@ -0,0 +1,5 @@
+import PopularConcert from "./PopularConcert";
+import PopularSeminar from "./PopularSeminar";
+import UpcomingEvent from "./UpcomingEvent";
+
+export{PopularSeminar,UpcomingEvent,PopularConcert}
