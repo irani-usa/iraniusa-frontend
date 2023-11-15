f0ffcb7f7a0b4f371b457e122c32dd09007b22aa
Milad Mohamadi <MiladDevarts@gmail.com>
2023-11-15 13:47:37 +0400

    fix: edit imported modules

--- app/profile/ultra/exhibition/page.tsx
+++ app/profile/ultra/exhibition/page.tsx
@@ -1,64 +1,92 @@
-import React from 'react'
+import React from "react";
 
-import About from '@/components/Profile/business/starter/About'
-import { Cover, Avatar, UserTitle, Blog, Navigation } from '@/components/Profile/general'
-import OverlayMenu from '@/components/Layout/OverlayMenu'
-import { Intro } from '@/components/Profile/ultra/exhibition/Intro'
-import Team from '@/components/Profile/business/pro/Team'
-import Social  from '@/components/Profile/ultra/architecture/Social'
-import Gallery from '@/components/Profile/A&E/pro/Gallery'
-import { Frequent } from '@/components/Profile/ultra/architecture/Frequent'
-import Terms from '@/components/Profile/business/pro/Terms'
-import {Company} from "@/components/Profile/Professional/Pro/index";
-import Forms from '@/components/Profile/ultra/exhibition/Forms'
+import About from "@/components/Profile/business/starter/About";
+import {
+  Cover,
+  Avatar,
+  UserTitle,
+  Blog,
+  Navigation,
+} from "@/components/Profile/general";
+import OverlayMenu from "@/components/Layout/OverlayMenu";
+import { Intro } from "@/components/Profile/ultra/exhibition/Intro";
+import Team from "@/components/Profile/business/pro/Team";
+import Social from "@/components/Profile/ultra/architecture/Social";
+import Gallery from "@/components/Profile/A&E/pro/Gallery";
+import { Frequent } from "@/components/Profile/ultra/architecture/Frequent";
+import Terms from "@/components/Profile/business/pro/Terms";
+import { Company } from "@/components/Profile/Professional/Pro/index";
+import Forms from "@/components/Profile/ultra/exhibition/Forms";
 
-import { Features } from '@/components/Profile/ultra/exhibition/Features'
-import { Artist } from '@/components/Profile/ultra/exhibition/Artist'
-import { Tickets } from '@/components/Profile/ultra/exhibition/Tickets'
-
-
-
+import { Features } from "@/components/Profile/ultra/exhibition/Features";
+import { Artist } from "@/components/Profile/ultra/exhibition/Artist";
+import { Tickets } from "@/components/Profile/ultra/exhibition/Tickets";
 
 const page = () => {
-    return (
-        <section>
-            <OverlayMenu />
-            <div className="w-full relative">
-                <Cover background={"/images/profile/Cover.png"} />
-            </div>
-            <div className="w-full container flex relative -top-[100px] left-0 p-10 mb-40 lg:mb-9">
-                <Avatar background={"/images/profile/Prof.png"} Contact={true} />
-                <UserTitle
-                    fullname={"Amin gholami"}
-                    jobtitle={"Product designer"}
-                    city={"Product designer"}
-                    isPro={true}
-                    blogLevel={"Active"}
-                    follower={"11.2 k"}
-                    following={"23"}
-                />
-            </div>
-            <div className="w-full h-1 border-t border-white-shade-4"></div>
-        <section className='lg:container lg:flex'>
-            <Navigation />
-        <section className='lg:border-l lg:border-white-shade-4 '>
-                <Intro about={'is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys'} cover={'/images/profile/ultra/Chair.png'} title={'Get The Tickets, Now Or Never'}  />
-                <About about={'Adidas has more than 20 years of experience in athletics industry through designing shoes and clothes also more than 20 collaborations wordwide '} city={' Los angless'} name={'Jason todd'} job={'Founder'} cover={'/images/blog/User.webp'} title={'What is our company all about?'} />
-                <Team />
-                <Features />
-                <Artist />
-                < Company title={'Salons we worked with'} /> 
-                <Tickets title={'Tickets and concerts'} />
-                <Gallery />
-                <Forms desc={''} />
-                <Social desc={'is simply dummy text of the printing and typesetting industry. Lorem Ipsum '} />
-                <Blog title={'Blog'} desc={'is simply dummy text of the printing and typesetting industry. Lorem Ipsum '} classes={'lg:pl-5'} />
-                <Frequent />
-                <Terms />
-            </section>
+  return (
+    <section>
+      <OverlayMenu />
+      <div className="w-full relative">
+        <Cover background={"/images/profile/Cover.png"} />
+      </div>
+      <div className="w-full container flex relative -top-[100px] left-0 p-10 mb-40 lg:mb-9">
+        <Avatar background={"/images/profile/Prof.png"} Contact={true} />
+        <UserTitle
+          fullname={"Amin gholami"}
+          jobtitle={"Product designer"}
+          city={"Product designer"}
+          isPro={true}
+          blogLevel={"Active"}
+          follower={"11.2 k"}
+          following={"23"}
+        />
+      </div>
+      <div className="w-full h-1 border-t border-white-shade-4"></div>
+      <section className="lg:container lg:flex">
+        <Navigation />
+        <section className="lg:border-l lg:border-white-shade-4 ">
+          <Intro
+            about={
+              "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys"
+            }
+            cover={"/images/profile/ultra/Chair.png"}
+            title={"Get The Tickets, Now Or Never"}
+          />
+          <About
+            about={
+              "Adidas has more than 20 years of experience in athletics industry through designing shoes and clothes also more than 20 collaborations wordwide "
+            }
+            city={" Los angless"}
+            name={"Jason todd"}
+            job={"Founder"}
+            cover={"/images/blog/User.webp"}
+            title={"What is our company all about?"}
+          />
+          <Team />
+          <Features />
+          <Artist />
+          <Company title={"Salons we worked with"} />
+          <Tickets title={"Tickets and concerts"} />
+          <Gallery />
+          <Forms desc={""} />
+          <Social
+            desc={
+              "is simply dummy text of the printing and typesetting industry. Lorem Ipsum "
+            }
+          />
+          <Blog
+            title={"Blog"}
+            desc={
+              "is simply dummy text of the printing and typesetting industry. Lorem Ipsum "
+            }
+            classes={"lg:pl-5"}
+          />
+          <Frequent />
+          <Terms />
         </section>
-        </section>
-    )
-}
+      </section>
+    </section>
+  );
+};
 
-export default page
+export default page;
