d89de512b7e6057a9b77fe718d07d14006427b1b
Milad Mohamadi <MiladDevarts@gmail.com>
2023-12-10 19:04:06 +0400

    fix: add aos library

--- components/Pages/Home/Hero/Title.tsx
+++ components/Pages/Home/Hero/Title.tsx
@@ -1,14 +1,27 @@
 
-import React from 'react'
+"use client"
+
+import React, { useEffect } from 'react'
 import Separator from '../../../Separator'
 
+import Aos from "aos";
+import "aos/dist/aos.css";
+
 const Title = () => {
+
+  useEffect(() => {
+    Aos.init({
+      once: true,
+    });
+  }, []);
+
   return (
     <div className='flex flex-col items-center'>
       <Separator classes="mt-10 lg:mt-16" />
-      <h1 className='text-3xl lg:text-4xl mt-5 lg:mt-0 leading-8 text-black font-semibold lg:font-medium font-outfit text-center'>
+      <h1 data-aos="fade-up" className='text-3xl lg:text-4xl mt-5 lg:mt-0 leading-8 text-black font-semibold lg:font-medium font-outfit text-center'>
         Businesses and Professionals 
-        <br />
+      </h1>
+      <h1 data-aos="fade-up" data-aos-delay="500" className='text-3xl lg:text-4xl mt-5 lg:mt-0 leading-8 text-black font-semibold lg:font-medium font-outfit text-center'>
         from Thousands of Services
       </h1>
     </div>
