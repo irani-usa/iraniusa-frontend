db2b954fa0e9c07e495ec274333f645cd7bd1c06
Your Name <arashlatifioo44@gmail.com>
2024-11-09 15:20:16 +0330

    edit brands page profile

--- components/Profile/business/pro/MembersCard.tsx
+++ components/Profile/business/pro/MembersCard.tsx
@@ -12,12 +12,7 @@ interface MembersCardProps {
 
 const MembersCard: React.FC<MembersCardProps> = (props) => {
   return (
-    // <div  style={{ backgroundImage: `url(${props.background})` }}
-    // className={`${props.class} w-full h-64 bg-center bg-cover rounded-2xl flex flex-col p-5 items-start justify-end gap-y-1 mt-8 lg:max-w-[20%] lg:p-4 lg:h-60 lg:mt-0`}  >
-    //     <span className='text-white font-outfit font-semibold'>{props.fullname}</span>
-    //     <span className='font-opensans text-white-shade-8 text-sm'>{props.job}</span>
-    // </div>
-    <div className={`w-full
+       <div className={`w-full
       ${props.class} flex-col`}>
            <section className='border-[1.5px] p-4 bg-white  border-white-shade-4 
     flex items-center gap-x-2 rounded-2xl hover:bg-blue transition-all duration-200 group '>
---
+++ components/Profile/ultra/architecture/BranchesCard.tsx
@@ -0,0 +1,34 @@
+import { Button } from '@/components/Forms'
+import { Location } from '@/components/Icons/Outline'
+import Link from 'next/link'
+import React from 'react'
+
+
+interface BranchesCardProps {
+    title: string,
+    item:string[],
+    class:string,
+  }
+
+const BranchesCard: React.FC<BranchesCardProps> = (props) => {
+  return (
+       <div className={`w-full
+      ${props.class} flex-col`}>
+           <section className='border-[1.5px] p-4 bg-white  border-white-shade-4 rounded-2xl '>
+                  
+               <div className='flex flex-col justify-center '>
+                 <div className="flex w-full my-5 justify-center">
+                 <Location />
+                 </div>
+                   <h1 className='text-black font-semibold text-xl mb-4'>{props.title}</h1>
+                   {props.item.map(item => (
+                   <span className='text-white-shade-11 flex gap-1 mb-2'><Location />{item}</span>
+                   ))}
+                   <Button content='Navigate' classes='text-white py-3 mt-4' />       
+               </div>
+           </section>
+       </div>
+  )
+}
+
+export default BranchesCard
