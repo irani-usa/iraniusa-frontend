116d3bd7801e43f0144f028e5ed9939a0dc2f5ed
Your Name <arashlatifioo44@gmail.com>
2024-11-09 15:28:20 +0330

    edit A&E starter

--- components/Profile/A&E/starter/Gallery.tsx
+++ components/Profile/A&E/starter/Gallery.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import IconElement from '../../general/IconElement'
+import GalleryIcon from '@/components/Icons/Profile/Gallery'
 
 const Gallery = () => {
   return (
     <section className='w-full border-white-shade-4 border-b pt-8 lg:pb-7'>
       <div className='container  lg:pl-5'>
+      <IconElement icon={<GalleryIcon />}/>
+
         <h1 className="text-[27px] font-outfit text-center font-bold text-black lg:text-3xl mt-5 lg:py-8">Gallery</h1>
         <div className='grid grid-cols-1 items-center gap-x-5 gap-y-4 lg:grid-cols-2 py-9 mt-5'>
          
