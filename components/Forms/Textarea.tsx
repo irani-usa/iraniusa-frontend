5f09f8afe2fd44c3c9bdabfd1ac6837d4464946f
abolfazl <abolfazlghotilo@gmail.com>
2025-04-27 22:06:30 +0330

    fixed contact page

--- components/Forms/Textarea.tsx
+++ components/Forms/Textarea.tsx
@@ -11,9 +11,9 @@ const Textarea: React.FC<TextareaProps> = (props) => {
   return (
     <>
           <div>
-          <label className='block text-lg font-bold py-3 text-black' htmlFor={props.id}>{props.label}</label>
+          <label className='block text-lg font-semibold py-3 text-blue' htmlFor={props.id}>{props.label}</label>
             
-            <textarea className={`${props.classes} w-full text-black px-3 py-3 font-normal rounded-xl bg-white-shade-3 focus:outline-hidden focus:shadow-none`} rows={4} id={props.id} placeholder={props.placeHolder}>
+            <textarea className={`${props.classes} w-full h-44 text-white-shade-15 px-3 py-3 font-medium font-inter rounded-xl bg-transparent border border-white-shade-4 focus:outline-hidden focus:shadow-none`} rows={4} id={props.id} placeholder={props.placeHolder}>
                 
             </textarea>
           </div>
--- components/Icons/Solid/User.tsx
+++ components/Icons/Solid/User.tsx
@@ -8,8 +8,8 @@ interface UserProps {
 const User: React.FC<UserProps> = (props) => {
     return (
         <svg className={props.classes} width="30" height="26" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path className='group-hover:stroke-white' d="M10.5734 10C12.8746 10 14.7401 8.13454 14.7401 5.83335C14.7401 3.53217 12.8746 1.66669 10.5734 1.66669C8.27222 1.66669 6.40674 3.53217 6.40674 5.83335C6.40674 8.13454 8.27222 10 10.5734 10Z" stroke="black" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
-            <path className='group-hover:stroke-white' d="M17.7317 18.3333C17.7317 15.1083 14.5234 12.5 10.5734 12.5C6.62337 12.5 3.41504 15.1083 3.41504 18.3333" stroke="black" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+            <path className='group-hover:stroke-white' d="M10.5734 10C12.8746 10 14.7401 8.13454 14.7401 5.83335C14.7401 3.53217 12.8746 1.66669 10.5734 1.66669C8.27222 1.66669 6.40674 3.53217 6.40674 5.83335C6.40674 8.13454 8.27222 10 10.5734 10Z" stroke="#a6a6a6" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
+            <path className='group-hover:stroke-white' d="M17.7317 18.3333C17.7317 15.1083 14.5234 12.5 10.5734 12.5C6.62337 12.5 3.41504 15.1083 3.41504 18.3333" stroke="#a6a6a6" strokeWidth="1.2" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
     )
 }
