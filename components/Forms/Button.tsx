c134f4d0c9ccbc0423b338bc9629d130050cf724
abolfazl <abolfazlghotilo@gmail.com>
2025-05-03 19:21:02 +0330

    fixed search bar of faq

--- components/Forms/Button.tsx
+++ components/Forms/Button.tsx
@@ -9,7 +9,7 @@ interface ButtonProps {
 
 const Button: React.FC<ButtonProps> = (props) => {
   return (
-    <button onClick={props.onClick} className={`${props.classes} font-inter text-white text-lg font-medium rounded-2xl w-full text-center border-2 border-black bg-blue hover:bg-white/0 transition-all duration-300 hover:text-black`}>{props.content}</button>
+    <button onClick={props.onClick} className={`${props.classes} w-[30%] font-inter text-white text-lg font-medium rounded-2xl text-center border-2 border-black bg-blue hover:bg-white/0 transition-all duration-300 hover:text-black`}>{props.content}</button>
   )
 }
 
