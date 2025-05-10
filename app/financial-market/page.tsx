cf1e97b635ee2451acfbe18bb5e24e32f72ff5f8
abolfazl <abolfazlghotilo@gmail.com>
2025-05-10 17:04:32 +0330

    fixed add titles line of financial-market page

--- app/financial-market/page.tsx
+++ app/financial-market/page.tsx
@@ -38,9 +38,9 @@ const page = () => {
                 stroke="#A6A6A6"
               />
             </svg>
-            All markets & stocks
+            Markets & Stocks
           </h1>
-          <h1 className="text-black text-3xl font-bold font-outfit lg:text-5xl lg:max-w-md ">
+          <h1 className="text-black text-3xl font-bold font-outfit lg:text-4xl lg:max-w-md ">
             Reliable international Financial markets
           </h1>
           <p className="text-white-shade-9 font-opensans lg:max-w-md">
@@ -49,7 +49,7 @@ const page = () => {
           </p>
          
         </div>
-        <div className="lg:flex items-center gap-x-4">
+        <div className="mt-24 lg:mt-0 lg:flex items-center gap-x-4">
           <div>
             <Financial
               content={"Total shares"}
@@ -60,21 +60,58 @@ const page = () => {
               found={"-1,10%"}
               iconComp={<Apple />}
               frame={""}
-              info={false}
-            />
+              info={false} chart={<svg width="103" height="32" viewBox="0 0 103 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M10.7146 16.3242L4.95012 29.8046C4.71634 30.3513 4.17904 30.7059 3.58445 30.7059H100.515C101.335 30.7059 102 30.0409 102 29.2206V27.0969C102 26.2766 101.335 25.6116 100.515 25.6116H100.008C99.4143 25.6116 98.8776 25.2579 98.6433 24.7123L93.9862 13.8663C93.7957 13.4227 93.4014 13.0993 92.929 12.9993L88.94 12.1547C88.5529 12.0728 88.2146 11.8398 87.9999 11.5075L84.9982 6.86026C84.3735 5.89297 82.9332 5.97378 82.4206 7.00489L78.5634 14.7631C78.3567 15.1788 77.9675 15.4739 77.5114 15.5609L72.7719 16.464C72.4615 16.5231 72.1403 16.4817 71.8551 16.3458L63.2663 12.2544C62.9571 12.1071 62.7083 11.8575 62.5621 11.5479L58.0776 2.0534C57.7265 1.30993 56.8381 0.99321 56.0958 1.34682L52.0334 3.28201C51.6909 3.4452 51.4237 3.73331 51.2869 4.08723L47.5681 13.7041C47.423 14.0791 47.1322 14.3793 46.7619 14.5361L42.1665 16.482C42.052 16.5305 41.9441 16.5931 41.8453 16.6685L36.9249 20.4187C36.4065 20.8138 35.6912 20.8248 35.161 20.4459L32.2991 18.4009C31.7193 17.9866 30.9269 18.0425 30.411 18.5341L27.3878 21.4144C26.8938 21.885 26.1428 21.9586 25.5669 21.5928L16.2175 15.6544C15.9794 15.5032 15.7032 15.4229 15.4211 15.4229H12.0803C11.4857 15.4229 10.9484 15.7775 10.7146 16.3242Z" fill="url(#paint0_linear_252_3539)"/>
+                <path d="M1 30.7059H3.58445C4.17904 30.7059 4.71634 30.3513 4.95012 29.8046L10.7146 16.3242C10.9484 15.7775 11.4857 15.4229 12.0803 15.4229H15.4211C15.7032 15.4229 15.9794 15.5032 16.2175 15.6544L25.5669 21.5928C26.1428 21.9586 26.8938 21.885 27.3878 21.4144L30.411 18.5341C30.9269 18.0425 31.7193 17.9866 32.2991 18.4009L35.161 20.4459C35.6912 20.8248 36.4065 20.8138 36.9248 20.4187L41.8453 16.6685C41.9441 16.5931 42.052 16.5305 42.1665 16.482L46.7619 14.5361C47.1322 14.3793 47.423 14.0791 47.5681 13.7041L51.2869 4.08723C51.4237 3.73331 51.6909 3.4452 52.0334 3.28201L56.0958 1.34682C56.8381 0.99321 57.7265 1.30993 58.0776 2.0534L62.5621 11.5479C62.7083 11.8575 62.9571 12.1071 63.2663 12.2544L71.8551 16.3458C72.1403 16.4817 72.4615 16.5231 72.7718 16.464L77.5114 15.5609C77.9675 15.4739 78.3567 15.1788 78.5634 14.7631L82.4206 7.00489C82.9332 5.97378 84.3734 5.89297 84.9982 6.86026L87.9999 11.5075C88.2146 11.8398 88.5529 12.0728 88.94 12.1547L92.929 12.9993C93.4014 13.0993 93.7957 13.4227 93.9862 13.8663L98.6433 24.7123C98.8776 25.2579 99.4143 25.6116 100.008 25.6116H102" stroke="#58BD7D" stroke-width="1.48529" stroke-linecap="round" stroke-linejoin="round"/>
+                <defs>
+                <linearGradient id="paint0_linear_252_3539" x1="52.0941" y1="-54.1997" x2="51.5548" y2="30.7063" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#45B36B"/>
+                <stop offset="1" stop-color="#45B36B" stop-opacity="0"/>
+                </linearGradient>
+                </defs>
+                </svg>
+                }        />
             <Financial
               content={"Price"}
               content2={"24h%"}
-              classes={
-                "bg-transparent text-black border-[1px] border-white-shade-4 lg:my-0"
-              }
+              classes={"bg-transparent text-black border-[1px] border-white-shade-4 lg:my-0"}
               frame={"border-[1.5px] border-white-shade-4"}
               title={"BTC"}
               price={"$36,641.20"}
               found={"+6.04%"}
               iconComp={<Btc />}
-              info={false}
-            />
+              info={false} chart={<svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="101"
+                height="31"
+                viewBox="0 0 101 31"
+                fill="none"
+              >
+                <path
+                  d="M1 30H3.84393C4.2577 30 4.65191 29.8239 4.92808 29.5158L10.5824 23.2073C10.6393 23.1438 10.6906 23.0754 10.7355 23.0029L14.788 16.4679C15.2132 15.7824 16.1146 15.5732 16.7982 16.0014L25.0896 21.1955C25.6503 21.5468 26.3773 21.4758 26.8595 21.0227L29.8348 18.2269C30.3385 17.7536 31.1055 17.6996 31.6705 18.0978L34.7596 20.2749C35.1365 20.5405 35.6171 20.6119 36.0549 20.4672L41.2188 18.7614L46.0587 16.6672C46.183 16.6135 46.2992 16.5426 46.4039 16.4567L51.5197 12.2603C52.0523 11.8235 52.8181 11.8196 53.355 12.2511L57.4978 15.58C57.7257 15.7631 58.0036 15.8734 58.2951 15.8965L63.6484 16.3194H70.1854C70.6261 16.3194 71.0431 16.1198 71.3194 15.7765L76.7203 9.06804C76.7713 9.00474 76.8274 8.94582 76.8882 8.89189L84.9362 1.75315C85.7227 1.05544 86.9669 1.39602 87.2884 2.39708L90.0705 11.059C90.232 11.5617 90.6529 11.9375 91.1705 12.0413L95.0546 12.8199C95.6927 12.9478 96.1683 13.4836 96.2198 14.1322L96.9818 23.7343C97.0419 24.4913 97.6738 25.075 98.4332 25.075H100"
+                  stroke="#58BD7D"
+                  strokeWidth="1.45588"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M3.84391 30.0014H1L98.5388 30.3586C99.3449 30.3616 100 29.7089 100 28.9028V26.5321C100 25.728 99.3482 25.0762 98.5441 25.0762H98.4332C97.6738 25.0762 97.0419 24.4925 96.9818 23.7355L96.2198 14.1328C96.1683 13.4842 95.6927 12.9484 95.0547 12.8205L91.1705 12.0419C90.6529 11.9381 90.232 11.5622 90.0705 11.0596L87.2884 2.39718C86.9669 1.39609 85.7227 1.0555 84.9361 1.75325L76.8882 8.8923C76.8274 8.94623 76.7713 9.00516 76.7203 9.06846L71.3194 15.7773C71.0431 16.1205 70.6261 16.3202 70.1854 16.3202H63.6484L58.2951 15.8972C58.0036 15.8742 57.7257 15.7639 57.4978 15.5807L53.355 12.2516C52.8181 11.8202 52.0522 11.824 51.5197 12.2609L46.4039 16.4575C46.2992 16.5434 46.183 16.6142 46.0587 16.668L41.2188 18.7623L36.055 20.4682C35.6171 20.6129 35.1365 20.5415 34.7596 20.2758L31.6705 18.0987C31.1055 17.7005 30.3385 17.7544 29.8348 18.2277L26.8595 21.0237C26.3773 21.4768 25.6503 21.5478 25.0896 21.1965L16.7983 16.0022C16.1147 15.5739 15.2132 15.7831 14.788 16.4687L10.7355 23.004C10.6906 23.0765 10.6393 23.1449 10.5824 23.2084L4.92808 29.5172C4.65192 29.8254 4.25769 30.0014 3.84391 30.0014Z"
+                  fill="url(#paint0_linear_771_5606)"
+                />
+                <defs>
+                  <linearGradient
+                    id="paint0_linear_771_5606"
+                    x1="51.0823"
+                    y1="-54.1383"
+                    x2="50.542"
+                    y2="30.0017"
+                    gradientUnits="userSpaceOnUse"
+                  >
+                    <stop stopColor="#45B36B" />
+                    <stop offset="1" stopColor="#45B36B" stopOpacity="0" />
+                  </linearGradient>
+                </defs>
+              </svg>}            />
           </div>
           <img
             src="/images/financial/chart.webp"
@@ -83,7 +120,6 @@ const page = () => {
           />
         </div>
       </section>
-      <section></section>
       <section>
         <div className="container lg:hidden">
           <Chart />
@@ -92,7 +128,10 @@ const page = () => {
           </div>
           <div className="mt-14">
             <div className="flex items-center justify-between py-7">
-              <h1 className="font-outfit text-black font-semibold text-lg">
+            <h1 className="flex items-center gap-2 font-outfit text-black font-semibold text-2xl">
+              <svg width="21" height="1" viewBox="0 0 21 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <line y1="0.5" x2="21" y2="0.5" stroke="#D9D9D9"/>
+              </svg>
                 Crypto Currency
               </h1>
               <span className="font-opensans text-white-shade-11">See all</span>
@@ -101,7 +140,10 @@ const page = () => {
           </div>
           <div className="mt-14">
             <div className="flex items-center justify-between py-7">
-              <h1 className="font-outfit text-black font-semibold text-lg">
+            <h1 className="flex items-center gap-2 font-outfit text-black font-semibold text-2xl">
+              <svg width="21" height="1" viewBox="0 0 21 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <line y1="0.5" x2="21" y2="0.5" stroke="#D9D9D9"/>
+              </svg>
                 Crypto Currency
               </h1>
               <span className="font-opensans text-white-shade-11">See all</span>
@@ -110,8 +152,11 @@ const page = () => {
           </div>
           <div className="mt-14">
             <div className="flex items-center justify-between py-7">
-              <h1 className="font-outfit text-black font-semibold text-lg">
-                Commodities
+            <h1 className="flex items-center gap-2 font-outfit text-black font-semibold text-2xl">
+              <svg width="21" height="1" viewBox="0 0 21 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <line y1="0.5" x2="21" y2="0.5" stroke="#D9D9D9"/>
+              </svg>
+              Commodities
               </h1>
               <span className="font-opensans text-white-shade-11">See all</span>
             </div>
@@ -119,8 +164,11 @@ const page = () => {
           </div>
           <div className="mt-14">
             <div className="flex items-center justify-between py-7">
-              <h1 className="font-outfit text-black font-semibold text-lg">
-                Fiat Currency
+            <h1 className="flex items-center gap-2 font-outfit text-black font-semibold text-2xl">
+              <svg width="21" height="1" viewBox="0 0 21 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <line y1="0.5" x2="21" y2="0.5" stroke="#D9D9D9"/>
+              </svg>
+              Fiat Currency
               </h1>
               <span className="font-opensans text-white-shade-11">See all</span>
             </div>
@@ -128,8 +176,11 @@ const page = () => {
           </div>
           <div className="mt-14">
             <div className="flex items-center justify-between py-7">
-              <h1 className="font-outfit text-black font-semibold text-lg">
-                Stock Market
+            <h1 className="flex items-center gap-2 font-outfit text-black font-semibold text-2xl">
+              <svg width="21" height="1" viewBox="0 0 21 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <line y1="0.5" x2="21" y2="0.5" stroke="#D9D9D9"/>
+              </svg>
+              Stock Market
               </h1>
               <span className="font-opensans text-white-shade-11">See all</span>
             </div>
@@ -165,7 +216,6 @@ const page = () => {
 <path d="M7.02957 36.3226C7.18704 35.8819 7.42386 35.5799 7.74003 35.4165C8.05468 35.2499 8.46299 35.2562 8.96494 35.4355L11.5171 36.3472L11.2203 37.1779L8.82363 36.3217C8.51963 36.2131 8.26991 36.1943 8.07446 36.2654C7.87665 36.3355 7.731 36.5014 7.63753 36.763C7.50195 37.1424 7.52429 37.448 7.70454 37.6798C7.88563 37.9092 8.1977 38.1031 8.64073 38.2613L10.5849 38.9558L10.2881 39.7865L6.37497 38.3886L6.60612 37.7418L7.17804 37.8146L7.19446 37.7686C7.08809 37.6349 7.01363 37.4901 6.97107 37.3341C6.92699 37.1748 6.91035 37.0094 6.92116 36.8379C6.9328 36.664 6.96894 36.4923 7.02957 36.3226Z" fill="white"/>
 <path d="M8.24143 49.4646L8.03048 52.5655L2.5553 52.1932L2.76625 49.0923L3.52274 49.1438L3.37293 51.3458L4.8597 51.4469L5.00007 49.3834L5.75282 49.4346L5.61244 51.4981L7.33139 51.615L7.48119 49.4129L8.24143 49.4646Z" fill="white"/>
 </svg>
-
 <span className="text-white text-xl font-medium text-center">The 1st multi-service Iranian-American platform</span>
             </div>
              </div>
@@ -176,7 +226,10 @@ const page = () => {
         <div className="container hidden lg:flex">
           <div className="w-full flex flex-col">
             <div className="flex items-center justify-between py-7">
-              <h1 className="font-outfit text-black font-semibold text-2xl">
+              <h1 className="flex items-center gap-2 font-outfit text-black font-semibold text-2xl">
+              <svg width="21" height="1" viewBox="0 0 21 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <line y1="0.5" x2="21" y2="0.5" stroke="#D9D9D9"/>
+              </svg>
                 Crypto Currency
               </h1>
               <span className="font-opensans text-white-shade-11 font-medium">
@@ -189,8 +242,11 @@ const page = () => {
         <div className="container hidden lg:flex">
           <div className="w-full flex flex-col">
             <div className="flex items-center justify-between py-7">
-              <h1 className="font-outfit text-black font-semibold text-2xl">
-                Precious Metals
+            <h1 className="flex items-center gap-2 font-outfit text-black font-semibold text-2xl">
+              <svg width="21" height="1" viewBox="0 0 21 1" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <line y1="0.5" x2="21" y2="0.5" stroke="#D9D9D9"/>
+              </svg>
+              Precious Metals
               </h1>
               <span className="font-opensans text-white-shade-11 font-medium">
                 See all
