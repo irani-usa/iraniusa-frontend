1ad443e6f20ee912ecf72d558294a73ceec0822c
abolfazl <abolfazlghotilo@gmail.com>
2025-05-13 16:54:09 +0330

    fixed forget-password

--- app/forget-password/page.tsx
+++ app/forget-password/page.tsx
@@ -9,22 +9,22 @@ import Logo from "@/components/Logo";
 const page = () => {
   return (
     <>
-      <section className="container py-12 lg:flex justify-between h-screen">
-        <div className="flex flex-col gap-y-20">
-          <div className="hidden lg:flex ">
+      <section className="container py-12 lg:flex justify-around h-screen">
+        <div className="flex flex-col items-center lg:items-start">
+          <div className="">
             <Logo width={90} height={90} />
           </div>
-          <section className="lg:w-[420px]">
-            <div className=" w-20 h-20 items-center justify-center bordered flex rounded-xl  mx-auto mb-6">
+          <section className="w-full lg:w-[420px]">
+            <div className=" w-16 h-16 items-center justify-center bordered flex rounded-xl  mx-auto mb-6">
               <Password />
             </div>
-            <h1 className="text-black text-3xl text-center font-bold font-outfit">
+            <h1 className="text-blue text-2xl text-center font-semibold font-outfit">
               Forgot password?
             </h1>
-            <h2 className="text-white-shade-8 text-center text-lg py-2 font-opensans">
+            <h2 className="text-white-shade-10 text-center text-lg py-2 font-medium font-inter">
               No worreis, we will send you reset email
             </h2>
-            <form action="#" className="flex flex-col gap-y-5 py-10">
+            <form action="#" className="flex flex-col gap-y-5 w-full">
               <Input
                 id={"email"}
                 inputType={"email"}
@@ -35,26 +35,26 @@ const page = () => {
               />
               <Button
                 content={"Reset password"}
-                classes={"bg-blue py-4 lg:bg-black"}
+                classes={"bg-blue py-3 lg:bg-black"}
               />
             </form>
             <Link
               href="/login"
-              className="block text-black font-opensans font-medium text-lg text-center"
+              className="block py-8 text-white-shade-8 font-opensans font-medium text-lg text-center"
             >
               Back to login page
             </Link>
           </section>
         </div>
         <div
-          className="w-2/4 h-full p-8 bg-cover rounded-3xl hidden lg:flex items-end"
+          className="w-2/4 h-[570px] p-4 bg-cover rounded-3xl hidden lg:flex items-end"
           style={{
-            backgroundImage: `url('/images/auth/forget-background.jpg')`,
+            backgroundImage: `url('/images/auth/register-background.webp')`,
           }}
         >
           <div className="flex flex-col gap-y-4 mb-2">
-            <span className="inline-flex justify-center items-center w-72 h-20 font-outfit font-medium text-xl  rounded-full bg-white/10 backdrop-blur-xl">Do games pay streamers?</span>
-            <p className="text-xl text-[#C6C6C6]">
+            <span className="inline-flex justify-center items-center w-72 h-20 font-outfit font-medium text-xl  rounded-full text-white bg-white-shade-10 backdrop-blur-3xl">Do games pay streamers?</span>
+            <p className="text-xl text-white-shade-8">
             Successful streamers can make $3,000 to $5,000 per month from their subscribers, with the top broadcasters earning substantially more.
             </p>
           </div>
--- components/Forms/Input.tsx
+++ components/Forms/Input.tsx
@@ -14,11 +14,11 @@ const Input: React.FC<InputProps> = (props) => {
     return (
         <>
 
-            <div>
+            <div className='w-full'>
                 <label className='block text-lg font-semibold font-outfit pt-8 pb-3 text-blue' htmlFor={props.id}>{props.label}</label>
                 <div className={`${props.classes} border border-white-shade-4 text-black font-opensans flex items-center gap-x-4 text-lg font-medium rounded-xl w-full h-16 px-4`}>
                     {props.iconComp}
-                    <input className={`${props.classes} text-white-shade-15 text-sm w-full h-full rounded-xl bg-transparent focus:outline-hidden focus:shadow-none`} id={props.id} type={props.inputType} placeholder={props.placeHolder} />
+                    <input className={`${props.classes} pl-4 border-l border-l-white-shade-9 text-white-shade-15 text-sm w-full bg-transparent focus:outline-hidden focus:shadow-none`} id={props.id} type={props.inputType} placeholder={props.placeHolder} />
                 </div>
             </div>
         </>
