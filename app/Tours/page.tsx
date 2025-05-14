94b41b2ebb50e904bde75a081c5a18c0f357b241
abolfazl <abolfazlghotilo@gmail.com>
2025-05-14 15:32:02 +0330

    fixed tours page

--- app/Tours/page.tsx
+++ app/Tours/page.tsx
@@ -255,11 +255,46 @@ const ToursSearch = () => {
                 </div>
 
             </div>
-         <div className='w-full flex flex-col lg:grid lg:grid-cols-3 gap-5 my-8'>
+                          <section className="flex justify-center gap-x-3 my-12">
+                <div className="flex items-center justify-center border-[1px] border-white-shade-4 py-5 px-6 rounded-full">
+                  <svg
+                    width="8"
+                    height="14"
+                    viewBox="0 0 8 14"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M6.59961 13.0667L1.39961 6.99998L6.59961 0.933317"
+                      stroke="black"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </div>
+                <div className="flex items-center justify-center border-[1px] border-white-shade-4 py-5 px-6 rounded-full">
+                  <svg
+                    width="8"
+                    height="14"
+                    viewBox="0 0 8 14"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M1.40039 0.93335L6.60039 7.00002L1.40039 13.0667"
+                      stroke="black"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </div>
+              </section>
+         <div className='w-full flex flex-col gap-y-16 lg:grid lg:grid-cols-3 gap-5 my-8'>
          <MediumBanner name={"Supplemental income"} />
          <MediumBanner name={"Supplemental income"} />
          <MediumBanner name={"Supplemental income"} />  
-           
                 </div>
 
         </section>
