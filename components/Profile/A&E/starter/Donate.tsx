7e1d19cfbe1e4b12ab8f7ec20a99f1a662bfdc9e
Your Name <arashlatifioo44@gmail.com>
2025-05-28 15:45:23 +0330

    foxid doate and profile experince

--- components/Profile/A&E/starter/Donate.tsx
+++ components/Profile/A&E/starter/Donate.tsx
@@ -8,12 +8,13 @@ interface DonateProps {
 
 const Donate: React.FC<DonateProps> = (props) =>  {
   return (
-    <div className='container py-9 flex flex-col gap-y-9 lg:pl-5'>
+    <div className='container py-9 flex flex-col gap-y-9 lg:pl-5 '>
            <h1 className="text-2xl text-black text-center font-outfit font-bold lg:text-3xl">Donating to artist</h1>
-        <div className='w-full bg-cover bg-center h-[290px] rounded-3xl p-7 lg:p-10 lg:h-auto' style={{ backgroundImage: `url('/images/profile/A&E/donate.png')` }}>
-            <h1 className='text-white font-semibold text-xl lg:text-2xl lg:max-w-sm'>You can donate to {props.artist} and support him financially</h1>
-            <button type='button' className='bg-white text-black font-medium py-4 px-9
-             mt-7 rounded-2xl text-lg lg:px-14 lg:mt-10'>Donate</button>
+        <div className='bg-[#FFC524]/5 bg-contain bg-no-repeat bg-right h-[290px] rounded-3xl p-7 lg:p-15 lg:h-auto' style={{ backgroundImage: `url('/images/profile/A&E/951487566ddaacc684cc319c555b38c9c2bfb6b5.jpg')` }}>
+            <h1 className='text-black font-semibold text-xl lg:text-2xl lg:max-w-sm'>You can donate to {props.artist} and support him financially</h1>
+            <button type='button' className='bg-[#FFC524] text-black font-medium py-3 
+            px-7
+             mt-5 rounded-2xl text-lg lg:px-14 lg:mt-7'>Donate</button>
         </div>
     </div>
   )
--- components/Profile/business/starter/Exprince.tsx
+++ components/Profile/business/starter/Exprince.tsx
@@ -16,7 +16,7 @@ const Exprince: React.FC<ExprinceProps> = (props) => {
         <IconElement icon={<ExperincesIcon />} />
         <h1 className="text-black font-medium text-2xl font-outfit mt-5 lg:text-3xl">Company's experiences </h1>
         <p className=" text-white-shade-9  font-opensans py-4 mt-3">{props.exprince} </p>
-        <div className="w-full flex justify-start">
+        <div className="w-full flex lg:justify-start justify-center my-10">
           <p className="text-lg text-black font-semibold font-opensans border border-white-shade-4 w-max px-5 py-1 rounded-lg flex items-center gap-2 ">
             <Calendar /> Founded in: Sep 2024
           </p>
